70034b4ba982410c01371c7f91f1836a11e8b2ee
Doğan Ay Şengül <20660110052@nevsehir.edu.tr>
2022-08-21 09:50:12 +0300

    trend post component yeniden yazılmaya başlandı

--- my-blog/resources/views/livewire/trend-post.blade.php
+++ my-blog/resources/views/livewire/trend-post.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <div class="postUp"><div><p>Öne Çıkan Yazılar</p></div></div>
-@foreach($posts as $post)
+    <div class="postUp"><div><p>Öne Çıkan Yazılar</p></div></div><!--
+@foreach($posts as $post) 
                     <div class="post">
                         <div class="icerik"
                         x-date
@@ -27,4 +27,27 @@
                             x-data="{url: '/blog/{{$post->id}}'}" @click="location.replace(url)"></div>
                     </div>
                     @endforeach
+-->
+    <div id="trendContainer">
+        <div id="trendFirstContainer">
+            <div id="trendFirstImg">
+                <img src="{{asset('test.jpg')}}" alt="">
+            </div>
+            <div id="trendFirstText">
+                <h3>Lorem ipsum dolor sit. </h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit atque beatae dicta. Ea accusantium omnis necessitatibus perspiciatis sint, eaque debitis.</p>
+            </div>
+        </div>
+        <div id="trendSecondContainer">
+            <div class="trendMiniPost">
+
+            </div>
+            <div class="trendMiniPost">
+                
+            </div>
+            <div class="trendMiniPost">
+                
+            </div>
+        </div>
+    </div>
 </div>
